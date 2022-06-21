d6ffd72e89344c853c15f42ae160620bd8959c2d
Ahmed-Raouf-Metwally <ahmedraouf.metwally@gmail.com>
2022-06-21 23:09:48 +0200

    home0000

--- modules/User/user.router.js
+++ modules/User/user.router.js
@@ -6,7 +6,7 @@ const UserRouter = require('express').Router();
 
 UserRouter.post('/logIn',SigninUser)
 UserRouter.post('/logout',SignOutUser)
-UserRouter.get('/',home)
+UserRouter.get('/app',home)
 
 
 
